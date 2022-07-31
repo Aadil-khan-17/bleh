67927f7da8cb5e577df122667a8bcd80503d451d
sneha-16hub <ishuvashu123@gmail.com>
2022-08-01 00:28:06 +0530

    Update settings.py

--- Backend/blockchain/blockchain/settings.py
+++ Backend/blockchain/blockchain/settings.py
@@ -130,10 +130,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static-files')
 MEDIA_URL = '/media/'
 
-# EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_USE_TLS = True
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'try802814@gmail.com'
-#  EMAIL_HOST_PASSWORD = '#'
-# EMAIL_USE_SSL = False
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = '#'
+EMAIL_HOST_PASSWORD = '#'
+EMAIL_USE_SSL = False
